05f4da8be53f5506ecef8af78c834429aa7071db
Esteban Martinez <esteban.mq09@gmail.com>
2021-04-07 17:26:57 +0200

    Initial commit

---
+++ app/src/main/java/com/app/taskjects/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.app.taskjects;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
---
+++ app/src/main/java/com/app/taskjects/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.app.taskjects;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+
+
+    //Duracion del spl
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen_taskjects_layout);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent loginScreen = new Intent(SplashScreen.this,LoginActivity.class);
+                SplashScreen.this.startActivity(loginScreen);
+                SplashScreen.this.finish();
+            }
+        },SPLASH_DISPLAY_LENGTH);
+
+
+    }
+}
